2fa8d8134d1e144231ef4f8263776fa8826602f7
hothitai2000 <116723332+hothitai2000@users.noreply.github.com>
2023-03-20 17:21:33 +0700

    tai done

--- index.php
+++ index.php
@@ -4,9 +4,48 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>TRAVEL DANA</title>
 </head>
 <body>
-    
+https://meet.google.com/kmy-ejhu-obr?authuser=0
+<h1>Tour Offers</h1>
+    <hr class="header-line">
+    <div class="container">
+        <div class="anhchung">
+            <div class="main-img">
+                <img src="https://6f3ebe2ff971707.cmccloud.com.vn/tour/wp-content/uploads/2021/12/hoi-an-da-nang.png" alt="">
+                
+                <div class="tai">
+                    <ul class="cha">
+                        <li>$303030</li>
+                    </ul>
+                    <a href="#">vua edit xong</a>
+                    <p>from:Ha Noi</p>
+                </div>
+            </div>
+
+            <div class="second-img">
+                <img src="https://6f3ebe2ff971707.cmccloud.com.vn/tour/wp-content/uploads/2021/12/hoi-an-da-nang.png" alt="">
+                <div class="tai">
+                    <ul class="cha">
+                        <li>$303030</li>
+                    </ul>
+                    <a href="#">vua edit xong</a>
+                    <p>from:Ha Noi</p>
+                </div>
+            </div>
+            <div class="third -img">
+                <img src="https://6f3ebe2ff971707.cmccloud.com.vn/tour/wp-content/uploads/2021/12/hoi-an-da-nang.png" alt="">
+                <div class="tai">
+                    <ul class="cha">
+                        <li>$303030</li>
+                    </ul>
+                    <a href="#">vua edit xong</a>
+                    <p>from:Ha Noi</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
