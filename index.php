d42c5f7465d5647ed18c43c98febc2e5f22d3e04
Ho Thi Bich <bich.ho24@student.passerellesnumeriques.org>
2023-03-27 20:37:26 +0700

    done bich home

--- index.php
+++ index.php
@@ -4,9 +4,184 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TRAVEL DANA</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+     integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+     crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <link rel="stylesheet" href="/TRAVEL/PHP-Project/css/style.css">
+    <link rel="stylesheet" href="https://fontawesome.com/icons/calendar-days?s=solid&f=classic">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <title>Home Travel</title>
 </head>
 <body>
-    
+  <!-- Start section Tour -->
+    <section class="Tour">
+      <div class="container">
+        <div id="Video">
+          <img class="photo-img"src="https://statics.vinpearl.com/du-lich-da-nang_1657939501.JPG" alt="">
+          <h2 >Take a Video Tour</h2>
+          <br> <br>
+          <h6 > Starttravels brings about the most memorable moments in life, the unprecedented <br>
+               impressions. We brings the beauty of Viet Nam being world-famous. Let's experience 
+               <br> througt our video and feel it.</h6>
+               <a href="https://www.youtube.com/watch?v=Au6LqK1UH8g"><img class="icon-play" src="/TRAVEL/PHP-Project/img/play-button (2).png" alt=""></a>
+      </div>
+
+      
+        <div class="page-heading">
+          <h2>Harzadous sports tour</h2>
+          <hr class="heading-line" />
+      </div>
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="card" style="width:350px">
+              <img class="card-img-top" src="http://thuvienhinhanh.net/wp-content/uploads/2016/05/hinh-anh-dep-ve-nha-trang-thanh-pho-bien-1.jpg" alt="Card image" style="width:70%">
+              <div class="card-body">
+                <h3 class="back-ground"><span class="flight-destination">Du Lich Nha Trang,Viet  Nam</span>|<span class="flight-type" style="font-size: 13px;"> ONEWAY FLIGHT</span></h3>
+                <div class=" flight-timing">
+                  <span><i class="fa fa-plane"></i></span><span class="date">Aug, 02-2017 </span>&ensp; <span><i class="fa fa-plane"></i></span><span class="date">Aug, 03-2017 </span>
+                </div>
+                <p class="price">$568.00<span class="pkg"> Avg/Person</span>&ensp; &ensp;&ensp; &ensp;&ensp;
+                  <span><i class="fa fa-star orange"></i></span>
+                  <span><i class="fa fa-star orange"></i></span>
+                  <span><i class="fa fa-star orange"></i></span>
+                  <span><i class="fa fa-star orange"></i></span>
+                  <span><i class="fa fa-star lightgrey"></i></span></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="card" style="width:350px">
+              <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVcpVNv02FheE_y3sDmvNJf9YkUzz9OP3HbJwyNEGyB9lm9tcEZWpmU0xheQwPrMI7KPU&usqp=CAU" alt="Card image" style="width:70%">
+              <div class="card-body">
+                <h3 class="back-ground"><span class="flight-destination">Du Lich Nha Trang,Viet  Nam</span>|<span class="flight-type" style="font-size: 13px;"> ONEWAY FLIGHT</span></h3>
+                <div class=" flight-timing">
+                  <span><i class="fa fa-plane"></i></span><span class="date">Aug, 02-2017 </span>&ensp; <span><i class="fa fa-plane"></i></span><span class="date">Aug, 03-2017 </span>
+                </div>
+                <p class="price">$568.00<span class="pkg"> Avg/Person</span>&ensp; &ensp;&ensp; &ensp;&ensp;
+                  <span><i class="fa fa-star orange"></i></span>
+                  <span><i class="fa fa-star orange"></i></span>
+                  <span><i class="fa fa-star orange"></i></span>
+                  <span><i class="fa fa-star orange"></i></span>
+                  <span><i class="fa fa-star lightgrey"></i></span></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="card" style="width:350px">
+              <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwe1xyqEkpekP-gedomhwqSt6Q4JszZlgYEw&usqp=CAU" alt="Card image" style="width:70%">
+              <div class="card-body">
+                <h3 class="back-ground"><span class="flight-destination">Du Lich Nha Trang,Viet  Nam</span>|<span class="flight-type" style="font-size: 13px;"> ONEWAY FLIGHT</span></h3>
+                <div class=" flight-timing">
+                  <span><i class="fa fa-plane"></i></span><span class="date">Aug, 02-2017 </span>&ensp; <span><i class="fa fa-plane"></i></span><span class="date">Aug, 03-2017 </span>
+                </div>
+                <p class="price">$568.00<span class="pkg"> Avg/Person</span>&ensp; &ensp;&ensp; &ensp;&ensp;
+                  <span><i class="fa fa-star orange"></i></span>
+                  <span><i class="fa fa-star orange"></i></span>
+                  <span><i class="fa fa-star orange"></i></span>
+                  <span><i class="fa fa-star orange"></i></span>
+                  <span><i class="fa fa-star lightgrey"></i></span></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br>
+        <div class="catalary-2">
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="card" style="width:350px">
+                <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWYGvpOsX-1dwzm9cBmGee7BoTuB3O5VzClg&usqp=CAU" alt="Card image" style="width:70%">
+                <div class="card-body">
+                  <h3 class="back-ground"><span class="flight-destination">Du Lich Qui Nhon,Viet  Nam</span>|<span class="flight-type" style="font-size: 13px;"> ONEWAY FLIGHT</span></h3>
+                  <div class=" flight-timing">
+                    <span><i class="fa fa-plane"></i></span><span class="date">Aug, 02-2017 </span>&ensp; <span><i class="fa fa-plane"></i></span><span class="date">Aug, 03-2017 </span>
+                  </div>
+                  <p class="price">$568.00<span class="pkg"> Avg/Person</span>&ensp; &ensp;&ensp; &ensp;&ensp;
+                    <span><i class="fa fa-star orange"></i></span>
+                    <span><i class="fa fa-star orange"></i></span>
+                    <span><i class="fa fa-star orange"></i></span>
+                    <span><i class="fa fa-star orange"></i></span>
+                    <span><i class="fa fa-star lightgrey"></i></span></p>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="card" style="width:350px">
+                <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUfCNC-qDg3i_ApHPTl0LXMn7OEJ-gTBWWyA&usqp=CAU" alt="Card image" style="width:70%">
+                <div class="card-body">
+                  <h3 class="back-ground"><span class="flight-destination">Du Lich Nha Trang,Viet  Nam</span>|<span class="flight-type" style="font-size: 13px;"> ONEWAY FLIGHT</span></h3>
+                  <div class=" flight-timing">
+                    <span><i class="fa fa-plane"></i></span><span class="date">Aug, 02-2017 </span>&ensp; <span><i class="fa fa-plane"></i></span><span class="date">Aug, 03-2017 </span>
+                  </div>
+                  <p class="price">$568.00<span class="pkg"> Avg/Person</span>&ensp; &ensp;&ensp; &ensp;&ensp;
+                    <span><i class="fa fa-star orange"></i></span>
+                    <span><i class="fa fa-star orange"></i></span>
+                    <span><i class="fa fa-star orange"></i></span>
+                    <span><i class="fa fa-star orange"></i></span>
+                    <span><i class="fa fa-star lightgrey"></i></span></p>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="card" style="width:350px">
+                <img class="card-img-top" src="https://dulichkhampha24.com/wp-content/uploads/2019/09/du-lich-ha-long.jpg" alt="Card image" style="width:70%">
+                <div class="card-body">
+                  <h3 class="back-ground"><span class="flight-destination">Du Lich Ha Long Bay,Viet  Nam</span>|<span class="flight-type" style="font-size: 13px;"> ONEWAY FLIGHT</span></h3>
+                  <div class=" flight-timing">
+                    <span><i class="fa fa-plane"></i></span><span class="date">Aug, 02-2017 </span>&ensp; <span><i class="fa fa-plane"></i></span><span class="date">Aug, 03-2017 </span>
+                  </div>
+                  <p class="price">$568.00<span class="pkg"> Avg/Person</span>&ensp; &ensp;&ensp; &ensp;&ensp;
+                    <span><i class="fa fa-star orange"></i></span>
+                    <span><i class="fa fa-star orange"></i></span>
+                    <span><i class="fa fa-star orange"></i></span>
+                    <span><i class="fa fa-star orange"></i></span>
+                    <span><i class="fa fa-star lightgrey"></i></span></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="view-all text-center">
+          <a href="homepage.html" class="btn btn-orange">View All </a>
+      </div>
+      </div>
+    </section>
+    <!-- End section Tour -->
+
+    <!-- Strart section highlights -->
+    <section id="highlights">
+      <div class="container">
+        <div class="car">
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="h-text">
+                <span class="high"><i class="fa-solid fa-plane-departure"></i></span>
+                <h3 class="highlight-box">2496</h3>
+              <p class="highlight">OUTSTANDING TOURS</p>
+              </div>
+              
+            </div>
+            <div class="col-sm-4">
+              <div class="h-text">
+                <span class="high"><i class="fa fa-ship"></i></span>
+                <h3 class="highlight-box">1906</h3>
+              <p class="highlight">WORLDWIDE CRUISE</p>
+              </div>
+              
+            </div >
+            <div class="col-sm-4">
+              <div class="h-text">
+                <span class="high"><i class="fa fa-taxi"></i></span>
+                <h3 class="highlight-box">2033</h3>
+              <p class="highlight">LUXURY CAR BOOKING</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 </body>
 </html>
