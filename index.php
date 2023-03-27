4872f3e172d384b611b7fc9a205b382a0f8c2831
hothitai2000 <116723332+hothitai2000@users.noreply.github.com>
2023-03-27 15:23:00 +0700

    done home

--- index.php
+++ index.php
@@ -1,51 +1,263 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>TRAVEL DANA</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Du lịch</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
 </head>
 <body>
-https://meet.google.com/kmy-ejhu-obr?authuser=0
-<h1>Tour Offers</h1>
-    <hr class="header-line">
-    <div class="container">
-        <div class="anhchung">
-            <div class="main-img">
-                <img src="https://6f3ebe2ff971707.cmccloud.com.vn/tour/wp-content/uploads/2021/12/hoi-an-da-nang.png" alt="">
-                
-                <div class="tai">
-                    <ul class="cha">
-                        <li>$303030</li>
-                    </ul>
-                    <a href="#">vua edit xong</a>
-                    <p>from:Ha Noi</p>
+    <div class="container-fluid">
+        <div class="blackground-fluid">
+           <div class="container">
+             <div class="row">
+            <div class="col-lg-3">
+                <div class="icon">
+                <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+                    <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
+                </svg>
                 </div>
+                <h2>Best Price Guarantee</h2>
+                <p>Our website brings you the best experiences and 
+                price .Our services are approved a resolution by Vietnam 
+                Travel Branch services with the most standardp </p>
+            </div>
+              <div class="col-lg-3">
+                <div class="icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-dash-fill" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zM6 9.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1H6z"/>
+                </svg>
+                </div>
+                <h2>Safe and Secure</h2>
+                <p>All your personal information,supported information 
+                will be kept and secured in the best way .Only you and 
+                can now.Your informations never have leaked out and lost <p>
             </div>
 
-            <div class="second-img">
-                <img src="https://6f3ebe2ff971707.cmccloud.com.vn/tour/wp-content/uploads/2021/12/hoi-an-da-nang.png" alt="">
-                <div class="tai">
-                    <ul class="cha">
-                        <li>$303030</li>
-                    </ul>
-                    <a href="#">vua edit xong</a>
-                    <p>from:Ha Noi</p>
+             <div class="col-lg-3">
+                <div class="icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">
+                    <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
+                </svg>
                 </div>
+                <h2>Best Travel Agents</h2>
+                <p>We have related to the best Travel Agency in Vietnam.
+                supported and admitted by many branchs , We have been 
+                improving and broading our activities<p>
             </div>
-            <div class="third -img">
-                <img src="https://6f3ebe2ff971707.cmccloud.com.vn/tour/wp-content/uploads/2021/12/hoi-an-da-nang.png" alt="">
-                <div class="tai">
-                    <ul class="cha">
-                        <li>$303030</li>
-                    </ul>
-                    <a href="#">vua edit xong</a>
-                    <p>from:Ha Noi</p>
+
+            <div class="col-lg-3">
+                <div class="icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
                 </div>
+                <h2>Travel Guidelines</h2>
+                <p>All your questions will be replied by administrators and  
+                They will help you . We're always side and help you 
+                            About any problems<p>
             </div>
+            
+           </div>  
+               <div class="containers">
+                  <h2>Tour Offers</h2>
+                  <hr class="heading-line">
+                  <div class="row">
+                    <div class="col-lg-4">
+                        <div class="card" >
+                            <img class="card-img-top" src="https://media.baodansinh.vn/baodansinh/Images/2016/01/22/tranhuyenbtv/dang_nui1.jpg" alt="Card image">
+                            <div class="card-body">
+                                <h3 class="title">$32000</h3>
+                              <h4 class="card-title">Đỉnh Langbiang</h4>
+                              <p class="card-text">From: Da Lat</p>
+                              <div class="star">
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+
+                              </div>
+                             
+                              
+                            </div>
+                          </div>
+
+                    </div>
+                    
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <img class="card-img-top" src="https://i1-vnexpress.vnecdn.net/2022/11/17/Ve-may-bay-di-sai-gon-600x399-4356-2813-1668672299.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=8All1Mq-so56XkVbZXvdFA" alt="Card image">
+                            <div class="card-body">
+                                <h3 class="title">$1000000</h3>
+                              <h4 class="card-title">Sông Sài Gòn</h4>
+                              <p class="card-text">From: Ho Chi Minh city</p>
+                              <div class="star">
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="far fa-star"></i>
+
+                              </div>
+                             
+                              
+                            </div>
+
+                    </div>
+                  </div>
+                  <div class="col-lg-4">
+                    <div class="card">
+                        <img class="card-img-top" src="https://cdn3.ivivu.com/2013/09/du-lich-ha-long-ivivu-4.jpg" alt="Card image">
+                        <div class="card-body">
+                            <h3 class="title">$20</h3>
+                          <h4 class="card-title">Vịnh Hạ Long</h4>
+                          <p class="card-text">From: Quang Ninh</p>
+                          <div class="star">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+
+                          </div> 
+                        </div>
+                        
+
+                </div>
+
+                
+                
+              </div>
+
+               </div>
+               <button class="btn-btn-btn" type="button">View All</button>
+            </div>     
+           
         </div>
-    </div>
+      
+        
+     </div>
+   
+ </div>
+
+
+ <div class="ba ">
+    <h2>Cruise Offers</h2>
+    <hr class="header-line">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="text-body">
+					<div class="text">
+						<div class="card-body">
+							<h4 class="card-title">Chua Linh Ung Pogoda</h4>
+							<p class="card-text">From: DN-HA</p>
+						</div>
+						<div class="star">
+							<i class="fas fa-star"></i>
+							<i class="fas fa-star"></i>
+							<i class="fas fa-star"></i>
+							<i class="fas fa-star"></i>
+							<i class="far fa-star"></i>						
+						</div>
+                  <h2>$20000</h2>
+					</div>
+					<div class="img">
+                        
+						<img src="https://static.vinwonders.com/2022/09/Son-tra-peninsula-thumb.jpg" alt="" width="360px">
+                        <div class="t">
+                            <p>7 NIGHT , 6 DAY</p>
+                        </div>
+                        
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<div class="text-body">
+					<div class="text">
+						<div class="card-body">
+							<h4 class="card-title">Da Lat :Ga máng trượt Datanla</h4>
+							<p class="card-text">From: Da Lat</p>
+						</div>
+						<div class="star">
+							<i class="fas fa-star"></i>
+							<i class="fas fa-star"></i>
+							<i class="fas fa-star"></i>
+							<i class="fas fa-star"></i>
+							<i class="far fa-star"></i>
+            
+						</div>
+                    <h2>$90000</h2>
+					</div>
+					<div class="img">
+						<img src="https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1295,h_863/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/ctaysdn8ghxzq5zvaolv/KlookPass%C4%90%C3%A0L%E1%BA%A1t:ComboVuiCh%C6%A1i,Kh%C3%A1mPh%C3%A1%C4%90%C3%A0L%E1%BA%A1t.webp" alt="" width="290px">
+                        <div class="t">
+                            <p>7 NIGHT , 6 DAY</p>
+                        </div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="text-body">
+					<div class="text">
+						<div class="card-body">
+							<h4 class="card-title">Da Nang city:Ba Na Hills</h4>
+							<p class="card-text">From: DN-HA</p>
+						</div>
+						<div class="star">
+							<i class="fas fa-star"></i>
+							<i class="fas fa-star"></i>
+							<i class="fas fa-star"></i>
+							<i class="fas fa-star"></i>
+							<i class="far fa-star"></i>
+						</div>
+            <h2>$70000</h2>
+					</div>
+					<div class="img">
+						<img src="https://i1-dulich.vnecdn.net/2022/06/01/CauVangDaNang-1654082224-7229-1654082320.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=MeVMb72UZA27ivcyB3s7Kg" alt="" width="340px">
+                        <div class="t">
+                            <p>7 NIGHT , 6 DAY</p>
+                        </div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<div class="text-body" style="height: 230px;">
+					<div class="text">
+						<div class="card-body" style="width: 340px;">
+							<h4 class="card-title">Huế mộng huế mơ</h4>
+							<p class="card-text">From: DN-HA</p>
+						</div>
+						<div class="star">
+							<i class="fas fa-star"></i>
+							<i class="fas fa-star"></i>
+							<i class="fas fa-star"></i>
+							<i class="fas fa-star"></i>
+							<i class="far fa-star"></i>
+						</div>
+            <h2>$50000</h2>
+					</div>
+					<div class="img">
+						<img src="https://www.travellens.co/content/images/size/w2000/format/webp/2021/10/hue-vietnam.jpg" alt="" width="291px" height="230px" >
+                        <div class="t">
+                            <p>7 NIGHT , 6 DAY</p>
+                        </div>
+					</div>
+				</div>
+			</div>
+            
+		</div>
+        <button class="btn-btn-btn but" type="button">View All</button>
+	</div>
+ </div>
 </body>
-</html>
+</html> 
