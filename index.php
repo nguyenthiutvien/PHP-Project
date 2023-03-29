56133be7841ced742d7f7fc13589d78712b99f1e
hothiloan <loan.hothi24@student.passerellesnumeriques.org>
2023-03-29 15:57:48 +0700

    done

--- index.php
+++ index.php
@@ -1,12 +1,259 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TRAVEL DANA</title>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    
+    <section class="packages" id="packages">
+        <div class="container">
+            <h1>CAP TOUR</h1>
+            <div class="box-container">
+                <div class="box">
+                    <img src="http://cdn.tgdd.vn/Files/2021/07/03/1365421/10-dia-diem-du-lich-quang-tri-dang-di-nhat-202206071023104118.jpg" alt="">
+                    <div class="content">
+                        <h3 class="title-content">$30</h3>
+                        <h3> <i class="fas fa-map-marker-alt"></i> QUANG TRI </h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, nam!</p>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="box">
+                    <img src="https://cdnimgen.vietnamplus.vn/t620/uploaded/wbxx/2022_03_18/golden_bridge.jpg" alt="">
+                    <div class="content">
+                        <h3 class="title-content">$3000</h3>
+                        <h3> <i class="fas fa-map-marker-alt"></i> DA NANG </h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, nam!</p>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="box">
+                    <img src="https://vietnam.travel/sites/default/files/inline-images/11125-Qu%E1%BA%A3ng%20Nam-huybank%40gmail.com-hoi%20an%20ve%20dem%20.jpg" alt="">
+                    <div class="content">
+                        <h3 class="title-content">$6000</h3>
+                        <h3> <i class="fas fa-map-marker-alt"></i> BINH DINH </h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, nam!</p>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="view-all text-center">
+             <a class="btn btn-orange">View All</a>
+            </div>
+        </div>
+    </section>
+    <section id="testimonials" class="section-padding back-size">
+        <div class="container">
+            <div class="text-center" data-wow-delay="0.1s">
+                <h1 class="mb-5">TESTIMONIALS</h1>
+                <hr class="heading-line" />
+            </div>
+            <div class="row g-4 ">
+                <div class="col-lg-3 col-md-6" data-wow-delay="0.1s">
+                    <div class="lo-item text-center">
+                        <div class="rounded-circle m-4">
+                            <img class="img-fluid" src="https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2017/8/28/551691/Du-Lich_1-01.jpg" alt="">
+                        </div>
+                        <h5 class="mb-0">Vien  Nguyen</h5>
+                        <div class="rating">
+                            <span><i class="fa fa-star orange"></i></span>
+                            <span><i class="fa fa-star orange"></i></span>
+                            <span><i class="fa fa-star orange"></i></span>
+                            <span><i class="fa fa-star orange"></i></span>
+                            <span><i class="fa fa-star lightgrey"></i></span>
+                        </div>
+                        <small class="coment">Great outing</small>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 " data-wow-delay="0.3s">
+                        <div class="lo-item text-center ">
+                            <div class="rounded-circle m-4">
+                                <img class="img-fluid" src="https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2017/8/28/551691/Du-Lich_1-01.jpg" alt="">
+                            </div>
+                            <h5 class="mb-0">Loan Ho</h5>
+                            <div class="rating">
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star lightgrey"></i></span>
+                            </div>
+                            <small class="coment">Great outing</small>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6" data-wow-delay="0.5s">
+                        <div class="lo-item text-center">
+                            <div class="rounded-circle m-4">
+                                <img class="img-fluid" src="https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2017/8/28/551691/Du-Lich_1-01.jpg" alt="">
+                            </div>
+                            <h5 class="mb-0">Tai Ho</h5>
+                            <div class="rating">
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star lightgrey"></i></span>
+                            </div>
+                            <small class="coment">Great outing</small>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 " data-wow-delay="0.7s">
+                        <div class="lo-item text-center ">
+                            <div class="rounded-circle m-4">
+                                <img class="img-fluid" src="https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2017/8/28/551691/Du-Lich_1-01.jpg" alt="">
+                            </div>
+                            <h5 class="mb-0">Bich Ho</h5>
+                            <div class="rating">
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star lightgrey"></i></span>
+                            </div>
+                            <small class="coment">Great outing</small>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 " data-wow-delay="0.3s">
+                        <div class="lo-item text-center ">
+                            <div class="rounded-circle m-4">
+                                <img class="img-fluid" src="https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2017/8/28/551691/Du-Lich_1-01.jpg" alt="">
+                            </div>
+                            <h5 class="mb-0">Khanh Phung</h5>
+                            <div class="rating">
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star orange"></i></span>
+                                <span><i class="fa fa-star lightgrey"></i></span>
+                            </div>
+                            <small class="coment">Great outing</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+    <section id="latest-blog" class="section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="page-heading">
+                        <h1>OUR LATEST BLOGS</h1>
+                        <hr class="heading-line" />
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 col-md-4">
+                            <div class="main-block latest-block">
+                                <div class="main-img latest-img">
+                                    <a><img src="http://dulichtoday.vn/wp-content/uploads/2017/04/vinh-Ha-Long.jpg" class="img-responsive" alt="blog-img" /></a>
+                                </div>
+                                <div class="latest-info">
+                                    <ul class="list-unstyled">
+                                        <li><span><i class='fa fa-calendar'></i></span>29 April,2020<span class="author">by:Vien Nguyen </span></li>
+                                    </ul>
+                                </div>
+                                <div class="main-info latest-desc">
+                                    <div class="row">
+                                        <div class="col-xs-10 col-sm-10 main-title">
+                                            <a>Travel Insuranve Benefits</a>
+                                            <p>Travel Insurance is the kind of insuarance pays the expenses and damages that concern the travels.</p>
+                                        </div>
+                                    </div>
+                                    <i class="fa fa-angle-right"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4">
+                            <div class="main-block latest-block">
+                                <div class="main-img latest-img">
+                                    <a>
+                                        <img src="https://thuonggiaonline.vn/tgo-media/2020/5/25/Hoi_An.jpg" class="img-responsive" alt="blog-img" />
+                                    </a>
+                                </div>
+                                <div class="latest-info">
+                                    <ul class="list-unstyled">
+                                        <li>
+                                            <span><i class="fa fa-calendar" aria-hidden="true"></i></span>11 April,2020<span class="author">by:Tai HO</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="main-info latest-desc">
+                                    <div class="row">
+                                        <div class="col-xs-10 col-sm-10 main-title">
+                                            <a>Travel Guideline Agents</a>
+                                            <p>Travel Insurance is the kind of insuarance pays the expenses and damages that concern the travels.</p>
+                                        </div>
+                                    </div>
+                                    <i class="fa fa-angle-right"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4">
+                            <div class="main-block latest-block">
+                                <div class="main-img latest-img">
+                                    <a>
+                                        <img src="https://marketingtoancau.com/files/news/top-khu-du-lich-sinh-thai-dong-hoi-quang-binh-czrakaal.jpg" class="img-responsive" alt="blog-img" />
+                                    </a>
+                                </div>
+                                <div class="latest-info">
+                                    <ul class="list-unstyled">
+                                        <li><span><i class="fa fa-calendar"></i></span>09 April,2020<span class="author">by:Loan Ho</span></li>
+                                    </ul>
+                                </div>
+                                <div class="main-info latest-desc">
+                                    <div class="row">
+                                        <div class="col-xs-10 col-sm-10 main-title">
+                                            <a>Secure Travel Tips</a>
+                                            <p>Travel Insurance is the kind of insuarance pays the expenses and damages that concern the travels.</p>
+                                        </div>
+                                    </div>
+                                    <i class="fa fa-angle-right"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="view-all text-center">
+                        <a class="btn btn-orange">View All</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container-fluid">
+        <h2 class="title-item">Subscribe Our Newsletter</h2>
+        <p class="title-item-1">Subscibe to receive our interesting updates</p>
+        <img src="https://media.licdn.com/dms/image/C5116AQE0RigtbTNPmQ/profile-displaybackgroundimage-shrink_200_800/0/1554456570189?e=2147483647&v=beta&t=-HA-1WZ8gIKm2qKWNT6-CLDsqIbmvzq2OixPoQrC2PQ" width="100%" height="500px" />
+        <div class="form-group">
+            <input type="email" class="form-control input-lg inputemail" placeholder="Enter your email address">
+            <span class="input-group-btn iconemail"><button class="btn1 btn-lg"><i class="fa-solid fa-envelope"></i></button></span>
+        </div>
+    </div>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 </body>
 </html>
